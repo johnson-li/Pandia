a32d92d1dcc8cd3fc6589d730a8085b5a9659fbe
Li Xuebing <lix16@inari.comnet.aalto.fi>
2023-05-17 11:05:36 +0300

    update the start script

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 duration=30
-port=8888
+port=9999
 rid=''
-width=2160
-fps=10
+width=1080
+fps=30
 
 set -o errexit -o pipefail -o noclobber -o nounset
 ! getopt --test > /dev/null
@@ -57,25 +57,25 @@ gn gen out/Default --args='is_debug=true rtc_use_h264=true ffmpeg_branding="Chro
 ninja -C out/Default
 cd -
 
-sudo modprobe v4l2loopback devices=2
-sudo chown lix16:lix16 /dev/video1
+# sudo modprobe v4l2loopback devices=2
+# sudo chown lix16:lix16 /dev/video1
 rm /tmp/dump/* 2> /dev/null || true
 
 session=pandia
-tmux kill-session -t $session 2> /dev/null
+tmux kill-session -t $session 2> /dev/null || true
 tmux new-session -d -s $session
 for i in `seq 1 5`
 do
   tmux new-window -t ${session}:$i
 done
 # tmux send-key -t $session:0 "cd ~/Workspace/Pandia; python -m pandia.fakewebcam.main -w ${width} -f ${fps} > ~/Workspace/Pandia/results/fakewebcam.log" Enter
-tmux send-key -t $session:1 '~/Workspace/webrtc/src/out/Default/peerconnection_server' Enter
+tmux send-key -t $session:1 "~/Workspace/webrtc/src/out/Default/peerconnection_server --port ${port}" Enter
 echo 'Server started'
 sleep .1
-tmux send-key -t $session:2 '~/Workspace/webrtc/src/out/Default/peerconnection_client_headless --name receiver --receiving_only true --force_fieldtrials=WebRTC-FlexFEC-03-Advertised/Enabled/WebRTC-FlexFEC-03/Enabled/ 2> ~/Workspace/Pandia/results/receiver.log' Enter
+tmux send-key -t $session:2 "~/Workspace/webrtc/src/out/Default/peerconnection_client_headless --port ${port} --name receiver --receiving_only true --force_fieldtrials=WebRTC-FlexFEC-03-Advertised/Enabled/WebRTC-FlexFEC-03/Enabled/ 2> ~/Workspace/Pandia/results/receiver.log" Enter
 echo 'Receiver started'
 sleep 3
-tmux send-key -t $session:3 '~/Workspace/webrtc/src/out/Default/peerconnection_client_headless --name sender --autocall true --force_fieldtrials=WebRTC-FlexFEC-03-Advertised/Enabled/WebRTC-FlexFEC-03/Enabled/ 2> ~/Workspace/Pandia/results/sender.log' Enter
+tmux send-key -t $session:3 "~/Workspace/webrtc/src/out/Default/peerconnection_client_headless --port ${port} --width ${width} --fps ${fps} --name sender --autocall true --force_fieldtrials=WebRTC-FlexFEC-03-Advertised/Enabled/WebRTC-FlexFEC-03/Enabled/ 2> ~/Workspace/Pandia/results/sender.log" Enter
 echo 'Sender started'
 tmux send-key -t $session:4 'nload lo' Enter
 tmux send-key -t $session:5 '' Enter
