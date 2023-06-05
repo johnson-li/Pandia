dcf23b4ea02912f59edcae6c7a62b203ae9cab1f
Johnson <johnsonli1993@gmail.com>
2023-06-06 00:21:25 +0300

    fix random traffic control

--- scripts/random_traffic_control.sh
+++ scripts/random_traffic_control.sh
@@ -3,7 +3,7 @@
 period=10
 
 delay_list=(10 20 50 80 100 200 500)
-bw_list=(100 200 500 800 1000 2000 5000 8000 10000 20000 50000 80000 100000 200000 500000) 
+bw_list=(100 200 500 800 1000 2000 5000 8000 10000 20000 50000 80000 100000 200000 500000)
 
 delay_size=${#delay_list[@]}
 bw_size=${#bw_list[@]}
@@ -15,7 +15,7 @@ do
     bw_index=$(($RANDOM % $bw_size))
     delay=${delay_list[$delay_index]}
     bw=${bw_list[$bw_index]}
-    ~/Workspace/Pandia/scripts/start_traffic_control.sh --delay $delay --bw $bw
+    ~/Workspace/Pandia/scripts/start_traffic_control.sh -d $delay -b $bw
     echo "delay: $delay, bw: $bw"
     sleep $period
 done
