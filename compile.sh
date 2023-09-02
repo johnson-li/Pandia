0878810d5f009be8725bf155d860a209a41f9e4b
Johnson <johnsonli1993@gmail.com>
2023-09-02 13:56:01 +0300

    update

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 cd ~/Workspace/webrtc/src
 # gn gen out/Default --args='is_debug=true rtc_use_h264=true ffmpeg_branding="Chrome" use_rtti=true'
-ninja -C out/Release
+gn gen out/Release --args='is_debug=false rtc_use_h264=true ffmpeg_branding="Chrome" use_rtti=true'
+ninja -C out/Release -j$(nproc)
 cd -
 
