33ee445fd4f6b76bfe1ef3cffd7ff275ad416a63
johnson <johnsonli1993@gmail.com>
2023-09-06 13:20:58 +0000

    support scale

--- compile.sh
+++ compile.sh
@@ -1,6 +1,52 @@
 #!/bin/bash
-# exit when any command fails
-set -e
+
+set -o errexit -o pipefail -o noclobber -o nounset
+! getopt --test > /dev/null
+scale=1
+LONGOPTS=scale
+OPTIONS=s:
+! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+    exit 2
+fi
+eval set -- "$PARSED"
+while true; do
+    case "$1" in
+        -s|--scale)
+            scale="$2"
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Programming error"
+            exit 3
+            ;;
+    esac
+done
+
+for i in `seq 2 ${scale}` 
+do
+    if grep -Fxq "  sender$i:" compose.yaml
+    then
+        echo sender$i is already in compose.yaml
+    else
+        echo Add sender$i to compose.yaml
+        echo "  receiver$i:" >> compose.yaml
+        echo "    <<: *receiver1" >> compose.yaml
+        echo "    hostname: receiver$i" >> compose.yaml
+        echo "  sender$i:" >> compose.yaml
+        echo "    <<: *sender1" >> compose.yaml
+        echo "    hostname: sender$i" >> compose.yaml
+        echo "    depends_on:" >> compose.yaml
+        echo "      - stun" >> compose.yaml
+        echo "      - receiver$i" >> compose.yaml
+        echo "      - sender_base" >> compose.yaml
+
+    fi
+done
 
 cd ~/Workspace/webrtc/src
 gn gen out/Default --args='is_debug=true rtc_use_h264=true ffmpeg_branding="Chrome" use_rtti=true rtc_use_x11=false'
